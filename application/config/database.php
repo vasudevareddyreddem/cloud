c2b911e015b2312529dab8d8d54a69b4246ad5ea
vasudevareddyreddem <vasudevareddy549@gmail.com>
2018-02-27 15:24:53 +0530

    databse  connection
    databse  connection

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'cloud',
+	'hostname' => '43.255.154.8',
+	'username' => 'cloudlive',
+	'password' => 'cloudlive@123',
+	'database' => 'cloud_live',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
